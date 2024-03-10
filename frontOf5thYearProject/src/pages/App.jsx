c044ef4ba65375cc4b1da14280bf4587df8e78ea
Mohammed Alakhras <MabdMAk@gmail.com>
2024-03-10 17:47:26 +0300

    Edit App.js

--- frontOf5thYearProject/src/pages/App.jsx
+++ frontOf5thYearProject/src/pages/App.jsx
@@ -1,7 +1,38 @@
+import { useNavigate } from "react-router-dom";
+
 function App() {
+  const navigate = useNavigate();
+
   return (
     <>
-      <div>app</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap:'10%',
+          margin: "10%",
+          padding: "10%",
+          backgroundColor:'#116a3b'
+
+        }}
+      >
+        <button 
+          onClick={() => {
+            navigate("start");
+          }}
+        >
+          Start Page
+        </button>
+
+        <button 
+          onClick={() => {
+            navigate("signup");
+          }}
+        >
+          Signup Page
+        </button>
+      </div>
     </>
   );
 }
