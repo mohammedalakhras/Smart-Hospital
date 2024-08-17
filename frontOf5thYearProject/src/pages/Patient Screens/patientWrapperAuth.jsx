0c9b6c98d599adb0d7462feafd2ce8f2a8fb89ed
hamde7 <hamdediab7@gmail.com>
2024-08-17 17:27:37 +0300

    mintance auth

--- frontOf5thYearProject/src/pages/Patient Screens/patientWrapperAuth.jsx
+++ frontOf5thYearProject/src/pages/Patient Screens/patientWrapperAuth.jsx
@@ -1,11 +1,23 @@
-
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'
 // eslint-disable-next-line react/prop-types
 export default function PatientWrapperAuth({children}) {
-    const nav = useNavigate();
-    const type = localStorage.getItem('type');
-    if(type==='doctor') nav('/doctor')
-        console.log(!(type==='doctor'))
+  const nav = useNavigate();
+  const type = localStorage.getItem("type");
+  console.log("dd",type)
+  async function testType() {
+    if (type === undefined) {
+      nav("/signIn");
+    }
+
+    if (type === "doctor") {
+      nav("/doctor");
+    }
+  }
+  useEffect(() => {
+    console.log("dd",type)
+      testType()
+  }, []);
   return (
     <>
     {!(type==='doctor') ? ( <div>{children}</div>) : <p>loding...</p>}
